d6dba2d594d87c3d725ad9e62c5ec4d2095f271b
DanHarrisStudent <32270584+DanHarrisStudent@users.noreply.github.com>
2017-11-21 14:58:06 +0000

    11/21/2017
    In class work. Checkpoints thought about but decided against.

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checkpoint : MonoBehaviour {
+
+    //Checkpoint system, User must enter collider to activate next checkpoint, 
+    //next partical system acitavtes after user hits collider.
+    
+    public GameObject Flare;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Flare.SetActive(false);
+
+
+        SceneManager.LoadScene(0);
+
+    }   
+    
+}
