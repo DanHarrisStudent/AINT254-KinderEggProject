3f28a688e61791784fbf5dbcb3c4e72368de814d
Dan-PC\Dan <daniel.harris-4@students.plymouth.ac.uk>
2018-01-12 15:07:34 +0000

    currentwork

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -14,7 +14,7 @@ namespace ISS
         private float m_speed = 5.0f;
 
         [SerializeField]
-        private float m_rotSpeed = 50.0f;
+        private float m_rotSpeed = 40.0f;
 
         [SerializeField]
         private float cameraDistance = 5.0f;
@@ -37,21 +37,20 @@ namespace ISS
         {
             m_transform = transform;
             m_rigidbody = GetComponent<Rigidbody>();
-
+            //Creates a center of mass
             m_rigidbody.centerOfMass = com;
         }
 
         // Update is called once per frame
         void LateUpdate()
         {
+            //Keeps the camera a specific distance away from the car
             Vector3 currenCamPos = camera.transform.position;
-
             Quaternion currenCamRot = camera.transform.rotation;
 
             float angle = gameObject.transform.rotation.eulerAngles.y;
 
             camera.transform.rotation = Quaternion.Lerp(currenCamRot, Quaternion.Euler(new Vector3(angleX, angle, 0.0f)), 0.1f);
-
             Vector3 newCamPos = transform.position + new Vector3(-cameraDistance * Mathf.Sin(angle * Mathf.Deg2Rad), camerHight, -cameraDistance * Mathf.Cos(angle * Mathf.Deg2Rad));
 
             camera.transform.position = Vector3.Lerp(currenCamPos, newCamPos, 0.1f);
@@ -61,29 +60,31 @@ namespace ISS
 
         void FixedUpdate()
         {
-
+            //Creates a raycast veriable from the car's transform
             RaycastHit hit;
             Ray groundRay = new Ray(m_transform.position, Vector3.down);
 
             //Checking if the players tires are on the gound
             if (Physics.Raycast(groundRay, out hit, m_rayDistance))
             {
-                Debug.Log("Ray Hitting Ground");
                 //If the player is moving forward greater than 0.7
                 if (m_rigidbody.velocity.magnitude > 0.7f)
                 {
-                    //Allows the player to turn
-                    //m_rigidbody.AddTorque(new Vector3(0.0f, m_rotSpeed * Input.GetAxis("Horizontal"), 0.0f));
-
+                    //Player is reversing, rotation in negative
                     if (Input.GetKey(KeyCode.S))
                     {
                         m_rigidbody.AddTorque(new Vector3(0.0f, -m_rotSpeed * Input.GetAxis("Horizontal"), 0.0f));
 
                     }
-                    //Player is driving forward
+                    //Player is driving forward, allows user to turn
                     else
                     {
                         m_rigidbody.AddTorque(new Vector3(0.0f, m_rotSpeed * Input.GetAxis("Horizontal"), 0.0f));
+                    }
+
+                    if (m_rigidbody.velocity.magnitude > 20f)
+                    {
+                        m_rigidbody.AddTorque(new Vector3(0.0f, m_rotSpeed * 1.01f * Input.GetAxis("Horizontal"), 0.0f));
                     }
 
                 }
@@ -91,7 +92,7 @@ namespace ISS
                 //If the player is reversing
                 if (Input.GetKey(KeyCode.S))
                 {
-                    m_rigidbody.AddForce(m_transform.forward * m_speed / 2 * Input.GetAxis("Vertical"));
+                    m_rigidbody.AddForce(m_transform.forward * m_speed / 4 * Input.GetAxis("Vertical"));
 
                 }
                 //Player is driving forward
@@ -99,9 +100,25 @@ namespace ISS
                 {
                     m_rigidbody.AddForce(m_transform.forward * m_speed * Input.GetAxis("Vertical"));
                 }
+
+                //Reversing on the controller
+                if (m_rigidbody.velocity.magnitude < 0f)
+                {
+                    Debug.Log("Reversing");
+
+                    m_rigidbody.AddForce(m_transform.forward * m_speed * Input.GetAxis("Vertical") / 2);
+
+                }
             }
 
+        }
 
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "NosPickUp")
+            {
+                m_rigidbody.AddForce(m_transform.forward * m_speed * 10);
+            }
         }
     }
 }
---
+++ Assets/Scripts/NosBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NosBehaviour : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player_1")
+        Destroy(gameObject, 0.2f);
+    }
+}
